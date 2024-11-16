a4c377bcf1f8f52a185d1af188163e79854e81cb
annagracehaukoos <annagracehaukoos@gmail.com>
2024-11-15 19:09:56 -0500

    added stuff from the video tutorial

--- whackamole.py
+++ whackamole.py
@@ -1,11 +1,11 @@
 import pygame
+import random
 
 # make a comment
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
@@ -14,7 +14,11 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(event.pos)
             screen.fill("light green")
+            pygame.draw.line(screen, "black", (32, 0), (32, 512))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
             pygame.display.flip()
             clock.tick(60)
     finally:
